a2f8bb73d321c15fc959d34fcf24835594f93939
Дмитрий <89045089+Malanavi@users.noreply.github.com>
2023-04-16 22:50:20 +0300

    Added manifest.user.js

---
+++ manifest.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name Watch and download on Shikimori
+// @namespace https://github.com/Malanavi/Watch-and-download-on-Shikimori
+// @description The addon adds the ability to watch and download anime on the Shikimori website.
+// @icon https://raw.github.com/Malanavi/Watch-and-download-on-Shikimori/main/images/icons/logo128.png
+// @author Malanavi
+// @homepageURL https://github.com/Malanavi/Watch-and-download-on-Shikimori
+// @version 1.1.0
+// @copyright 2023, Malanavi
+// @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
+
+
+// @match *://shikimori.me/*
+
+// @require https://raw.github.com/Malanavi/Watch-and-download-on-Shikimori/main/scripts/helpers/helpers.js
+// @require https://raw.github.com/Malanavi/Watch-and-download-on-Shikimori/main/scripts/helpers/shikimori.js
+
+// @require https://raw.github.com/Malanavi/Watch-and-download-on-Shikimori/main/scripts/torrent.js
+// @require https://raw.github.com/Malanavi/Watch-and-download-on-Shikimori/main/scripts/player.js
+
+// @run-at document-start
+
+
+// @grant none
+// ==/UserScript==
