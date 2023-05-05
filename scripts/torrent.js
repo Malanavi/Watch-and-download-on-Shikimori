6c9a1d7fed6b79feb0fcb8600e4e318d36961d97
Malanavi <89045089+Malanavi@users.noreply.github.com>
2023-05-05 20:42:39 +0300

    Added new links for downloading
    AniLibria (magnet link) and Erai-raws

--- scripts/torrent.js
+++ scripts/torrent.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 class Torrent {
   static {
@@ -10,60 +10,64 @@ class Torrent {
   static async #onViewChanged() {
     const match = Shikimori.isAnimePage(window.location);
     if (match) {
-      if (document.querySelector(".rutracker-link")) {
-        document.querySelector(".rutracker-link").remove();
-      }
+      const { id } = match.groups;
+      const nameOfAnime = Shikimori.getNameOfAnime(id);
+      const links = [
+        { name: "RuTracker", src: `https://rutracker.org/forum/tracker.php?nm=${nameOfAnime}` },
+        { name: "AniLibria-magnet", src: `${await AniLibria.getAnimeOnAnilibria('https://api.anilibria.tv/v3/title/search?search=' + nameOfAnime)
+            .then(data => {
+              return data.list[0].torrents.list[0].magnet;
+            })}`,
+        },
+        { name: "Erai-raws", src: `https://www.erai-raws.info/?s=${nameOfAnime}` },
+      ];
 
-      const animeId = match.groups.id;
+      document.querySelectorAll(".torrent-link").forEach(link => link.remove());
 
-      const nameOfAnime = Shikimori.getNameOfAnime(animeId);
-
-      const torrent = this.#createTorrent();
-      torrent.animeId = animeId;
-      torrent.src = `https://rutracker.org/forum/tracker.php?nm=${nameOfAnime}`;
-
-      const link = this.#createLink(torrent);
-
-      const block = this.#createBlock(link, torrent);
+      const blocks = links.map((link) => {
+        const createdLink = this.#createLink(link.name, link.src);
+        return this.#createBlock(createdLink);
+      });
 
       const elements = document.querySelectorAll(".subheadline");
-      var arr = [];
+      let before = null;
 
       for (let i = 0; i < elements.length; i++) {
-        if (elements[i].textContent.includes("on other sites") || elements[i].textContent.includes("На других сайтах")) {
-          arr.push(elements[i]);
+        if (
+          elements[i].textContent.includes("on other sites") ||
+          elements[i].textContent.includes("На других сайтах")
+        ) {
+          before = elements[i];
+          break;
         }
       }
-      const before = arr[0];
 
-      Helpers.insertAfter(block, before);
+      if (before) {
+        blocks.forEach((block) => {
+          Helpers.insertAfter(block, before);
+        });
+      }
     }
   }
 
-  static #createLink(torrent) {
+  static #createLink(name, src) {
     const link = document.createElement("a");
     link.className = "b-link";
-    link.text = "RuTracker";
+    link.text = name;
 
     const style = document.createElement("style");
-    style.textContent = ".b-external_link.rutracker .linkeable::before, .b-external_link.rutracker .none::before, .b-external_link.rutracker a::before, .b-external_link.rutracker span::before { content: ''; height: 19px; margin-right: 6px; width: 19px; background-size: 19px 19px; background-color: rgba(0, 0, 0, 0); background-image: url('https://raw.github.com/Malanavi/Watch-and-download-on-Shikimori/main/images/icons/rutracker-favicon.ico'); }";
+    style.textContent = `.b-external_link.${name.toLowerCase()} .linkeable::before, .b-external_link.${name.toLowerCase()} .none::before, .b-external_link.${name.toLowerCase()} a::before, .b-external_link.${name.toLowerCase()} span::before { content: ''; height: 19px; margin-right: 6px; width: 19px; background-size: 19px 19px; background-color: rgba(0, 0, 0, 0); background-image: url('https://raw.github.com/Malanavi/Watch-and-download-on-Shikimori/main/images/icons/${name.toLowerCase()}-favicon.ico'); }`;
     document.head.appendChild(style);
 
-    link.href = `${torrent.src}`;
+    link.href = src;
     link.target = `_blank`;
 
     return link;
   }
 
-  static #createTorrent() {
-    const torrent = document.createElement("div");
-
-    return torrent;
-  }
-
   static #createBlock(link) {
     const block = document.createElement("div");
-    block.className = "b-external_link rutracker b-menu-line rutracker-link";
+    block.className = `b-external_link ${link.text.toLowerCase()} b-menu-line torrent-link`;
 
     block.appendChild(link);
 
